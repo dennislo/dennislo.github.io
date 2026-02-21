2160a88ed15defdfb46ccdedb18521231b028de4
dennislo <lo.dennis@gmail.com>
2026-02-21 17:11:02 +0000

    format

--- src/context/ThemeContext.test.tsx
+++ src/context/ThemeContext.test.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {render, screen, waitFor} from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import {getTimeBasedTheme, ThemeProvider, useTheme} from "./ThemeContext";
-import {mockDate} from "../test/test-utils";
+import { getTimeBasedTheme, ThemeProvider, useTheme } from "./ThemeContext";
+import { mockDate } from "../test/test-utils";
 
 const TestComponent = () => {
   const { theme, toggleTheme } = useTheme();
